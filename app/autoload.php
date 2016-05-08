58fcea05dc4bc577b02a559740b76990ffd9f964
Adrián Eduardo Galindo García <adriwrr@gmail.com>
2016-05-08 16:37:52 -0500

    Corrige acentos en crearTratamiento.tex y añade mongo al autoload

--- app/autoload.php
+++ app/autoload.php
@@ -9,5 +9,7 @@ use Composer\Autoload\ClassLoader;
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-
+// Anotaciones para MongoDB
+use Doctrine\ODM\MongoDB\Mapping\Driver\AnnotationDriver;
+AnnotationDriver::registerAnnotationClasses();
 return $loader;
