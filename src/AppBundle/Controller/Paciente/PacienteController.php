9fdf117abc9b669381c6db00c08e9ed34e1fa185
RubMurga <ruben.murga.d@gmail.com>
2016-05-28 12:53:13 -0500

    agregando alta paciente lado paciente

--- src/AppBundle/Controller/Paciente/PacienteController.php
+++ src/AppBundle/Controller/Paciente/PacienteController.php
@@ -5,7 +5,7 @@ namespace AppBundle\Controller\Paciente;
 
 use AppBundle\Document\Paciente\Paciente;
 use AppBundle\Document\User\User;
-use AppBundle\Form\Type\Paciente\PacienteType;
+use AppBundle\Form\Type\Paciente\PacienteRegistrationType;
 use AppBundle\Form\Type\User\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Core\SecurityContextInterface;
@@ -15,37 +15,34 @@ class PacienteController extends Controller
 {
     public function registrarPacienteAction(Request $request)
     {
-        $paciente = new Paciente();
         $usuario = new User();
 
-        $formPaciente = $this->createForm(new PacienteType(), $paciente);
-        $formUsuario = $this->createForm(new RegistrationType(), $usuario);
+        $formUsuario = $this->createForm(new PacienteRegistrationType(), $usuario);
 
-        $formPaciente->handleRequest($request);
         $formUsuario->handleRequest($request);
 
         if($formUsuario->isValid()){
             $dm = $this->get('doctrine_mongodb')->getManager();
             $usuario->setEnabled(true);
             $usuario->addRole("ROLE_PACIENTE");
-            $usuario->setUsername("-");
+            $usuario->setUsername($usuario->getPaciente()->getNombre() . " " . $usuario->getPaciente()->getApellidos());
             $dm->persist($usuario);
             $dm->flush();
 
-            if($formPaciente->isValid()){
-                $paciente->setEmail($usuario->getEmail());
-                $dm->persist($paciente);
-                $dm->flush();
-                return $this->redirect($this->generateUrl('home'));
-            }
+
+            return $this->redirect($this->generateUrl('registrar_paciente'));
+
         }
+        $request->getSession()->getFlashBag()->add(
+            'error',
+            'Datos incorrectos, por favor intenta nuevamente'
+        );
 
         return $this->render(
-            ':Paciente/registrar:registrar_paciente.html.twig',
+            ':Paciente/registrar:registrarPaciente.html.twig',
             array(
-                'formPaciente' => $formPaciente->createView(),
-                'formUsuario'=> $formUsuario->createView()
-                )
+                'formUsuario' => $formUsuario->createView()
+            )
         );
     }
 
